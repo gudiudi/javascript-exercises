b2032b8cab31d171eefcd5c983734c48a7997f8b
a3490620 <197717697+a3490620@users.noreply.github.com>
2025-03-03 07:33:21 +0700

    feat: pass the reverse string test using two pointer technique

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  let strArr = str.split('');
+  let left = 0;
+  let right = str.length - 1;
 
+  while (left <= right) {
+    leftSubStr = strArr[left];
+    strArr[left] = strArr[right];
+    strArr[right] = leftSubStr;
+    left++;
+    right--;
+  }
+
+  return strArr.join('');
 };
 
 // Do not edit below this line
