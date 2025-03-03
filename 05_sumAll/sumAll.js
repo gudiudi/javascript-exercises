f6537104d42c331b85d7cdf6dfdf768002091df5
a3490620 <197717697+a3490620@users.noreply.github.com>
2025-03-03 08:04:01 +0700

    feat: returns the sum of every integer between (and including) them

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(from, to) {
+  if (from < 0 || to < 0) return 'ERROR'; 
+  if (!Number.isInteger(from) || !Number.isInteger(to)) return 'ERROR';
 
+  let sum = 0;
+
+  if (from < to) {
+    while (from <= to) {
+      sum += from;
+      from++;
+    }
+  } else {
+    while (to <= from) {
+      sum += to;
+      to++;
+    }
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
