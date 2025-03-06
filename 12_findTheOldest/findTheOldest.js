47bfe60be650e816d7d61f7740b5a8b77292cd63
a3490620 <197717697+a3490620@users.noreply.github.com>
2025-03-06 18:32:21 +0700

    feat: implement the function with .map and .sort

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  const thisYear = new Date().getFullYear();
+  
+  return people
+    .map((x) => ((!x.yearOfDeath) ? {...x, yearOfDeath: thisYear} : x))
+    .sort((a, b) => (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth))[0]
 };
 
 // Do not edit below this line
