f777c362c7e9e7263110077890b27420217e87a2
a3490620 <197717697+a3490620@users.noreply.github.com>
2025-03-06 18:05:24 +0700

    feat: implemented fibonnaci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+  index = Number(index);
 
+  if (index < 0) return "OOPS"; 
+  if (index === 0) return index; 
+
+  let arr = [0, 1];
+  let prev = 0;
+  let current = 1;
+  for (let i = 2; i <= index; i++) {
+    let fib = prev + current;
+    prev = current;
+    current = fib;
+    arr.push(fib);
+  }
+  return arr[arr.length - 1];
 };
 
 // Do not edit below this line
