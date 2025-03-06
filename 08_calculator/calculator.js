5d74c69b2a35dd4c0d619fb2701a2c9992525958
a3490620 <197717697+a3490620@users.noreply.github.com>
2025-03-06 15:46:54 +0700

    feat: add calculator exercise solution by mainly using reduce

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  if (nums.length <= 0) return 0;
+	return nums.reduce(((a, b) => a + b));
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1;
+	return [...Array(num).keys()]
+    .map((i) => num - i)
+    .reduce((a, b) => a * b);
 };
 
 // Do not edit below this line
